bbb303fb151f09b8fe6ebbbe2ef6bd3a3d61630e
Mohan-Vinotharamkumar <navaneethamohan2002@gmail.com>
2023-09-22 02:02:13 +0530

    first commit

---
+++ workingKeyboard.js
@@ -0,0 +1,19 @@
+// document.addEventListener("keypress",function(event)
+// {
+//     key_press_event(event.key);
+// });
+
+
+// var length_char = document.querySelectorAll(".keys").length;
+
+
+// function key_press_event(key)
+// {
+//     alert(typeof(key))
+//     document.querySelector("."+key).classList.add("light");
+//     setTimeout(function()
+//     {
+//         document.querySelector("."+key).classList.remove("light")
+//     },300)
+// }
+
